00a806270443adf0bd8b081ea718d982f964af1d
moshe <github email address>
2017-04-05 17:26:15 -0400

    add a clouser function to index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 const app = "I don't do much."
+
+function firstPool(info) {
+  var pool_hight = 5
+
+  function pool1(hight) {
+    return hight < pool_hight
+  }
+  
+  
+  if(pool1(info)){
+  	return pool_hight;
+  } else {
+  	console.log("Sorry, you are not allowed at pool1!")
+  } 
+  
+
+  return {
+  	pool2,
+  	pool3
+  }
+}
